db9ded8714ae052e54c2a10d6cd9b733178b8b1a
flopie3000 <dehaan@dataprovider.com>
2026-05-09 13:24:56 +0200

    Fix routing for GitHub Pages

--- src/routes/routes.config.tsx
+++ src/routes/routes.config.tsx
@@ -3,13 +3,16 @@ import { App } from "../App";
 import { Home } from "../pages/Home/Home";
 import { Library } from "../pages/Library/Library";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: "library", element: <Library /> },
-    ],
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: "library", element: <Library /> },
+      ],
+    },
+  ],
+  { basename: import.meta.env.BASE_URL }
+);
